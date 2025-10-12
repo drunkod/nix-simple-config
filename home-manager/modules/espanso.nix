8a240238359377d0c1b3206c6ed054e56fc0aead
Alex Hp <drunkod@gmail.com>
2025-10-12 16:21:07 +0500

    fix

--- home-manager/modules/espanso.nix
+++ home-manager/modules/espanso.nix
@@ -1,8 +1,12 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+
+{
   services.espanso = {
     enable = true;
-        configs = {
-          default = {
+    
+    configs = {
+      default = {
+        # Search shortcut works fine
         search_shortcut = "ALT+SPACE";
         
         # Backend is correct for X11
@@ -19,115 +23,123 @@
         # Performance tuning
         pre_paste_delay = 100;
         clipboard_threshold = 100;
+      };
+    };
+    
     matches = {
-		base = {
-          matches = [
-            {
-              trigger = ":mydate";
-              replace = "{{mydate}}";
-              vars = [
-                {
-                  name = "mydate";
-                  type = "date";
-                  params = {
-                    format = "%d.%m.%Y";
-                  };
-                }
-              ];
-            }
+      base = {
+        matches = [
+          # Test trigger - try this first!
+          {
+            trigger = ":test";
+            replace = "Espanso is working! 🎉";
+          }
+          
+          {
+            trigger = ":mydate";
+            replace = "{{mydate}}";
+            vars = [
+              {
+                name = "mydate";
+                type = "date";
+                params = {
+                  format = "%d.%m.%Y";
+                };
+              }
+            ];
+          }
 
-            {
-              trigger = ":rand";
-              replace = "{{rand}}";
-              vars = [
-                {
-                  name = "rand";
-                  type = "random";
-                  params.choices = [
-		            "Hey there!"
-		            "Hello, how are you?"
-		            "Hi, nice to see you!"
-		            "Greetings!"
-		            "Hey, what's up?"
-		            "Hello there!"
-		            "Hi friend!"
-		          ];
-                }
-              ];
-            }
-          ];
-        };
+          {
+            trigger = ":rand";
+            replace = "{{rand}}";
+            vars = [
+              {
+                name = "rand";
+                type = "random";
+                params.choices = [
+                  "Hey there!"
+                  "Hello, how are you?"
+                  "Hi, nice to see you!"
+                  "Greetings!"
+                  "Hey, what's up?"
+                  "Hello there!"
+                  "Hi friend!"
+                ];
+              }
+            ];
+          }
+        ];
+      };
 
-        greethings = {
-          global_vars = [
-            {
-              name = "clipb";
-              type = "clipboard";
-			}
-            {
-              name = "quote";
-              type = "shell";
-              params = {
+      greethings = {
+        global_vars = [
+          {
+            name = "clipb";
+            type = "clipboard";
+          }
+          {
+            name = "quote";
+            type = "shell";
+            params = {
                 #                cmd = "curl -s 'https://zenquotes.io/api/random' | jq -r '.[0].q'";
                 #                cmd = "nix-shell -p neo-cowsay --run \"curl -s 'https://zenquotes.io/api/random' | jq -r '.[0].q' | cowthink\"";
-                cmd = "${lib.getExe pkgs.curl} -s https://zenquotes.io/api/random | ${lib.getExe pkgs.jq} -r '.[0].q' | ${pkgs.neo-cowsay}/bin/cowthink";
-              };
-            }
-          ];
-          matches = [
-            {
-              trigger = ":sg";
-              replace = "Sehr geehrter ";
-            }
-            {
-              triggers = [
-                ":omor"
-                ":gmo"
-              ];
-              replace = "Good morning from the office! 🌄🏢\n\n```\n{{quote}}\n```";
-            }
-            {
-              triggers = [
-                ":.omor"
-                ":.gmo"
-              ];
-              replace = "Good morning from the office! 🌄🏢";
-            }
-            {
-              triggers = [ ":gmho" ];
-              replace = "Good morning from home office! 🌄🏡\n\n```\n{{quote}}\n```";
-            }
-            {
-              triggers = [ ":.gmho" ];
-              replace = "Good morning from home office! 🌄🏡";
-            }
-            {
-              triggers = [
-                ":gna"
-                ":gnsg"
-              ];
-              replace = "Gute Nacht und schlaf gut! 🎑🌜🤗🌛🌃";
-            }
-            {
-              triggers = [ ":gmg" ];
-              replace = "Guten Morgen, wie hast du geschlafen? 🌄🤗☀️";
-            }
-            {
-              triggers = [ ":vd" ];
-              replace = "Vielen Dank!👍️";
-            }
-            {
-              triggers = [ ":ty" ];
-              replace = "Thank you! 👍️";
-            }
-            {
-              triggers = [ "эж" ];
-              replace = "{{clipb}}";
-            }
-          ];
-        };
-	};
+              cmd = "${lib.getExe pkgs.curl} -s https://zenquotes.io/api/random | ${lib.getExe pkgs.jq} -r '.[0].q' | ${pkgs.neo-cowsay}/bin/cowthink";
+            };
+          }
+        ];
+        
+        matches = [
+          {
+            trigger = ":sg";
+            replace = "Sehr geehrter ";
+          }
+          {
+            triggers = [
+              ":omor"
+              ":gmo"
+            ];
+            replace = "Good morning from the office! 🌄🏢\n\n```\n{{quote}}\n```";
+          }
+          {
+            triggers = [
+              ":.omor"
+              ":.gmo"
+            ];
+            replace = "Good morning from the office! 🌄🏢";
+          }
+          {
+            triggers = [ ":gmho" ];
+            replace = "Good morning from home office! 🌄🏡\n\n```\n{{quote}}\n```";
+          }
+          {
+            triggers = [ ":.gmho" ];
+            replace = "Good morning from home office! 🌄🏡";
+          }
+          {
+            triggers = [
+              ":gna"
+              ":gnsg"
+            ];
+            replace = "Gute Nacht und schlaf gut! 🎑🌜🤗🌛🌃";
+          }
+          {
+            triggers = [ ":gmg" ];
+            replace = "Guten Morgen, wie hast du geschlafen? 🌄🤗☀️";
+          }
+          {
+            triggers = [ ":vd" ];
+            replace = "Vielen Dank!👍️";
+          }
+          {
+            triggers = [ ":ty" ];
+            replace = "Thank you! 👍️";
+          }
+          {
+            triggers = [ "эж" ];
+            replace = "{{clipb}}";
+          }
+        ];
+      };
+    };
   };
-};
-};
 }
