6e4095d83cc0b0f333ecf9a220f4d6ad6ac8eab7
drunkod <drunkod@gmail.com>
2026-08-02 17:03:01 +0500

    home-manager: keep Alex profile user-specific

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,41 +1,13 @@
-{ config, pkgs, inputs, ... }:
+{ ... }:
 
 {
   imports = [
+    ./common.nix
     ./modules/espanso.nix
-    ./modules/proxy.nix
   ];
 
   home = {
     username = "alex";
     homeDirectory = "/home/alex";
-    stateVersion = "25.05";
-
-    packages = with pkgs; [
-      turbovnc
-      # Development
-      fabric-ai
-      vscode
-      windsurf
-      antigravity
-
-      # Browsers
-      google-chrome
-      #microsoft-edge
-
-      # Utilities
-      usbutils
-      pciutils
-      scrcpy
-      xarchiver
-      libnotify
-      sing-box
-      crow-translate
-      # inputs.codex-cli-nix.packages.${pkgs.system}.default
-      ripgrep
-      # claude-code
-    ];
   };
-
-  programs.home-manager.enable = true;
 }
